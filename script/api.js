0ae99414331ed53f0cdc8357f2a9fee6c739c840
Totenkopf <pysskuu88@icloud.com>
2022-11-06 19:31:12 +0300

    ДЗ часть 2. Выполнено на 1 балл

---
+++ script/api.js
@@ -0,0 +1,39 @@
+const CONFIG_API = {
+    url: 'http://sb-cats.herokuapp.com/api/2/37Evgeny',
+    headers: {
+        'Content-type': 'application/json'
+    }
+}
+
+class Api {
+    constructor(config){
+        this._url = config.url;
+        this._headers = config.headers;
+    }
+
+     // Метод  
+    _onRes(res){
+        return res.ok ? res.json() : Promise.reject({...res, message: "Server side error!!!"})
+    }
+
+    // Получаем всех котиков с сервера
+    getAllCats(){
+        // return вернуть цепочку промисов
+       return fetch(`${this._url}/show`, {
+            method: 'GET'
+        }).then(this._onRes)
+    }
+
+
+
+// Добавить котика на сервер
+    addNewCat(data){
+        return fetch(`${this._url}/add`, {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: this._headers
+        }).then(this._onRes)
+    }
+}
+
+const api = new Api(CONFIG_API);
--- script/index.js
+++ script/index.js
@@ -1,6 +1,8 @@
-const cardsContainer = document.querySelector('.cards')
+const cardsContainer = document.querySelector('.cards');
 const btnOpenPopup= document.querySelector('#add');
-const formCatAdd = document.querySelector('#popup-form-cat')
+const btnOpenAuthorization= document.querySelector('#login');
+const formCatAdd = document.querySelector('#popup-form-cat');
+const formAuthorization = document.querySelector('#form-login');
 
 function serializeForm(elem){
     const formData ={};
@@ -16,28 +18,69 @@ function serializeForm(elem){
     return formData;
 }
 
+
+// Функция добавления котика на страницу
 function formAddCat(event){
     event.preventDefault();
     const elementsFormCat = [...formCatAdd.elements];
     const dataFromForm = serializeForm(elementsFormCat);
-
-    const cardInst = new Card(dataFromForm, '#card-template');
-    const newCardElem= cardInst.getElement();
-    cardsContainer.append(newCardElem);
-
-    AddCat.close();
+    // добавление котика на сервер
+    api.addNewCat(dataFromForm)
+        .then(()=>{
+            const cardInst = new Card(dataFromForm, '#card-template');
+            const newCardElem= cardInst.getElement();
+            cardsContainer.append(newCardElem);
+            //  закрытие popup
+            AddCat.close();
+        })
 }
 
-cats.forEach(function(dataCat){
-    const cardInst = new Card(dataCat, '#card-template');
-    const newCardElem= cardInst.getElement();
-    cardsContainer.append(newCardElem);
-})
+//Функция Авторизации
+function formFromAuthorization(event){
+    event.preventDefault();
+    const elementsFormAuthorization = [...formAuthorization.elements];
+    const dataFromForm = serializeForm(elementsFormAuthorization);
+    Cookies.set('email', `email=${dataFromForm.email}`)
+    // удаляем скрытие с кнопки добавления котика
+    btnOpenPopup.classList.remove('visually-hidden')
+    // закрываем окно авторизации 
+    authorization.close();
+}
 
+
+// Получили всех котиков с сервера и отрендерили на странице
+api.getAllCats()
+    //  {data} получить данные по ключю
+    .then(({data})=>{
+        // Перебираем массив с котиками
+        data.forEach(function(dataCat){
+            // Создаем карточу
+            const cardInst = new Card(dataCat, '#card-template');
+            // Возвращаем разметку
+            const newCardElem= cardInst.getElement();
+            // Добавляем на страницу  
+            cardsContainer.append(newCardElem);
+        }) 
+    })
+
+
+// Вызов модельного окна для добавления котика
 const AddCat = new Popup('popup-add-cats')
-
 AddCat.setEventListener()
-
 btnOpenPopup.addEventListener('click',()=>  AddCat.open());
-
 formCatAdd.addEventListener('submit', formAddCat)
+
+// Вызов модельного окна авторизации
+const authorization = new Popup('authorization')
+authorization.setEventListener()
+btnOpenAuthorization.addEventListener('click',()=>  authorization.open());
+formAuthorization.addEventListener('submit', formFromAuthorization)
+
+const auth = Cookies.get('email');
+
+
+if(!auth){
+    authorization.open();
+    // вешаем ласс скрытия на кнопку добавления котика
+    btnOpenPopup.classList.add('visually-hidden')
+}
